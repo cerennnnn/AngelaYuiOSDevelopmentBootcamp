c2d7d7b4ef3b061f157bfecf7fc3c4f77d344afa
cerennnnn <cerengunnes@gmail.com>
2022-11-13 11:17:57 +0300

    Finish I Am Rich

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Ceren Güneş on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
