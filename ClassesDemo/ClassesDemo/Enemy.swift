705324d1752b66c228d8255277e4e87b3cf5483f
cerennnnn <cerengunnes@gmail.com>
2022-12-07 15:58:10 +0300

    BMI-Calculator project is completed!

---
+++ ClassesDemo/ClassesDemo/Enemy.swift
@@ -0,0 +1,8 @@
+//
+//  Enemy.swift
+//  ClassesDemo
+//
+//  Created by Ceren Güneş on 7.12.2022.
+//
+
+import Foundation
---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ClassesDemo
+//
+//  Created by Ceren Güneş on 7.12.2022.
+//
+
+import Foundation
+
+print("Hello, World!")
+
